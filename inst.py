08a1fc3168f1761abf0cb22d77682e882986d1c3
b2pacific <b2pacific@gmail.com>
2020-05-27 19:28:00 +0530

    Function to save Pictures

--- inst.py
+++ inst.py
@@ -5,12 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import time
+import urllib.request
 
 class Instabot:
     def __init__(self, username, pw):
         self.username=username
         self.pw=pw
         self.black = ["iiitbhopalmeme"]
+        self.save_list = ["dp._photos"]
         self.driver=webdriver.Firefox()
         self.driver.get("https://instagram.com")
         time.sleep(2)
@@ -19,27 +21,23 @@ class Instabot:
     def login(self):
         self.driver.implicitly_wait(10)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input").send_keys(self.username)
-        time.sleep(2)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input").send_keys(self.pw)
-        time.sleep(2)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button").click()
         time.sleep(2)
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/button[2]").click()
-        time.sleep(2)
 
     def like(self):
         self.driver.implicitly_wait(10)
 
         self.posts = self.driver.find_elements_by_xpath("//article[contains(@class, '_8Rm4L')]")
         self.heart_sec = self.driver.find_elements_by_xpath("//span[contains(@class, 'fr66n')]")
-        self.following_sec = self.driver.find_elements_by_xpath("//div[contains(@class, 'eleld')]")
 
-        for ele in self.posts:
-            self.heart=ele.find_element_by_class_name("eo2As").find_element_by_class_name("ltpMr").find_element_by_class_name("fr66n").find_element_by_tag_name("button")
-            self.colour=self.heart.find_element_by_tag_name("svg").get_attribute("fill")
-            self.username=ele.find_element_by_tag_name("header").find_element_by_class_name("o-MQd").find_element_by_class_name("RqtMr").find_element_by_class_name("e1e1d").find_element_by_tag_name("a").text
+        for post in self.posts:
+            self.heart = post.find_element_by_class_name("eo2As").find_element_by_class_name("ltpMr").find_element_by_class_name("fr66n").find_element_by_tag_name("button")
+            self.colour = self.heart.find_element_by_tag_name("svg").get_attribute("fill")
+            self.user = post.find_element_by_tag_name("header").find_element_by_class_name("o-MQd").find_element_by_class_name("RqtMr").find_element_by_class_name("e1e1d").find_element_by_tag_name("a").text
 
-            if self.blacklist(self.username):
+            if self.blacklist(self.user):
                 continue
             else:
                 if self.colour == "#262626":
@@ -47,6 +45,25 @@ class Instabot:
                 else:
                     break
 
+    def save(self):
+        self.driver.implicitly_wait(10)
+        print("1")
+        self.posts = self.driver.find_elements_by_xpath("//article[contains(@class, '_8Rm4L')]")
+
+        for post in self.posts:
+            self.user = post.find_element_by_tag_name("header").find_element_by_class_name("o-MQd").find_element_by_class_name("RqtMr").find_element_by_class_name("e1e1d").find_element_by_tag_name("a").text
+            print(self.user)
+            
+            if self.user in self.save_list:
+                print("2.5")
+                # if self.check_exists_by_class(post.find_element_by_class_name("_97aPb").find_element_by_tag_name("div"), "kPFhm"):
+                #      continue                    
+                # else:
+                print("3")
+                self.image_url = post.find_element_by_class_name("_97aPb").find_element_by_class_name("ZyFrc").find_element_by_class_name("eLAPa").find_element_by_class_name("KL4Bh").find_element_by_tag_name("img").get_attribute("src")
+                urllib.request.urlretrieve(self.image_url, "images/"+self.user+".jpg")
+                
+                break
 
     def blacklist(self, username):
         if username in self.black:
@@ -56,17 +73,18 @@ class Instabot:
 
 
 
-    def check_exists_by_xpath(self, xpath):
+    def check_exists_by_class(self, path, class_name):
         self.driver.implicitly_wait(10)
         try:
-            self.driver.find_element_by_xpath(xpath)
+            path.find_element_by_class_name(class_name)
         except NoSuchElementException:
-            return True
-        return False
+            return False
+        return True
 
 
 username = input("Enter your username: ")
-password = input("Enter your passwore: ")
+password = input("Enter your password: ")
 obj=Instabot(username, password)
 obj.login()
-obj.like()
+#obj.like()
+obj.save()
